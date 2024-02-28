bc043e4dd651294d74d0cd083745c13c566d33d8
abdulmuhyiismail <114898486+abdulmuhyiismail@users.noreply.github.com>
2024-02-28 17:39:54 +0700

    Add files via upload

---
+++ ObjekMobil.java
@@ -0,0 +1,8 @@
+public class ObjekMobil {
+    
+    public static void main(String[] args) {
+        MobilDiesel md = new MobilDiesel("Toyota", 600, 200,"solar");
+        md.infoMobilDiesel();
+    }
+    
+}
---
+++ mobil.java
@@ -0,0 +1,48 @@
+public class mobil {
+    String merk;
+    int harga, kecepatan;
+    
+    /**
+     * konstruktor
+     * Method yang namanya = Nama class
+     * Tidak boleh menggunakan void
+     * Tidak boleh ada return value
+     * Dijalankan hanya untuk 1x
+     * Digunakan untuk inisialisasi
+     */
+    mobil(String merk, int harga, int kecepatan){
+        this.merk = merk;
+        this.harga = harga;
+        this.kecepatan = kecepatan;
+    }
+    mobil(){
+        
+    }
+    void setMerk (String brand) {
+        merk = brand;
+    }
+
+    void setHargaKecepatan (int harga, int kecepatan) {
+        this.harga = harga;
+        this.kecepatan = kecepatan;
+    }
+    
+    String getMerk () {
+        return merk;
+    }
+    
+    int getHarga () {
+        return harga;
+    }
+    
+    int getKecepatan () {
+        return kecepatan;
+    }
+    
+    void infoMobil () {
+        System.out.println("Merk Mobil:" + getMerk());
+        System.out.println("Harga:" + getHarga());
+        System.out.println("Kecepatan:" + getKecepatan());
+        System.out.println("--------------------------:");
+    }
+}
